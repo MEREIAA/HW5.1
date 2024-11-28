cc82b8e798fb23fc3d4f22733f3e8e4ca35b9fe5
MEREIAA <mereiaalmazbekowa@gmail.com>
2024-11-28 20:22:55 +0600

    Second Push ыыdg

--- app/src/main/java/com/example/hw51/NextActivity.java
+++ app/src/main/java/com/example/hw51/NextActivity.java
@@ -3,6 +3,9 @@ package com.example.hw51;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -16,9 +19,11 @@ import com.google.android.material.slider.RangeSlider;
 import java.util.List;
 
 public class NextActivity extends AppCompatActivity {
+    private boolean isHeartPressed = false;
+    private boolean isCategoryPressed = false;
 
 
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint({"MissingInflatedId", "ResourceAsColor"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +33,22 @@ public class NextActivity extends AppCompatActivity {
         setContentView(R.layout.activity_next);
         resultTextView = findViewById(R.id.result_Text_View);
         RangeSlider rangeSlider = findViewById(R.id.range_seek_bar);
+        ImageView heartImageView = findViewById(R.id.heartImageView);
+
+        heartImageView.setOnClickListener(v -> {
+            if (isHeartPressed) {
+                heartImageView.setImageResource(R.drawable.heart);
+            } else {
+                heartImageView.setImageResource(R.drawable.heartpressed);
+            }
+            isHeartPressed = !isHeartPressed;
+        });
+
+
+        Spinner spinner = findViewById(R.id.dropdown_menu);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.choice_color, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
         rangeSlider.setValues(0f, 100f);
         double result = getIntent().getDoubleExtra("result", 0);
@@ -37,9 +58,21 @@ public class NextActivity extends AppCompatActivity {
         rangeSlider.addOnChangeListener((slider, value, fromUser) -> {
             List<Float> values = slider.getValues();
             System.out.println("Current values: " + values);
-
-            TextView buttonCategory = findViewById(R.id.button_category);
-            buttonCategory.setVisibility(View.GONE);
         });
+
+
+
+        TextView buttonCategory = findViewById(R.id.button_category);
+        buttonCategory.setOnClickListener(v -> {
+            if (isCategoryPressed) {
+                heartImageView.setImageResource(R.drawable.heart);
+                buttonCategory.setBackgroundResource(R.drawable.bg_button2);
+            } else {
+                heartImageView.setImageResource(R.drawable.heartpressed);
+                buttonCategory.setBackgroundResource(R.drawable.bg_button3);
+            }
+            isCategoryPressed = !isCategoryPressed;
+        });
+        buttonCategory.setVisibility(View.VISIBLE);
     }
 }
